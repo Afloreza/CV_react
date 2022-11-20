34c5b18fecf51706a972e07e14c2b6812a10b39b
Angela Florez <angelithafl98@gmail.com>
2022-11-20 13:27:47 -0500

    se adiciona datos de contacto

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -21,11 +21,8 @@ function Aux(){
                 <p className="mb-0">
                 Técnico laboral en sistemas, actualmente estudiante de Ingeniería de Sistemas
                 y Telecomunicaciones, Conocimientos ofimáticos en programas office, sistemas
-                operativos, sistema contable Visión. Conocimientos avanzados en ventas, planeación 
-                y cumplimiento de metas y capacidad de liderazgo. Persona con orientación a 
-                desarrollar la labor con calidad, innovadora, con iniciativa y colaboradora. 
-                Dispuesta a cumplir con las distintas funciones, obligaciones y normas laborales 
-                para así, formar parte del cumplimiento de metas planteadas por la organización.
+                operativos, sistema contable Contapyme, Agrowin y SAP BO. Conocimientos en bases de datos y seguridad informatica,
+                 dispuesta avanzar cada día y dar lo mejor de mi para cumplir las metas propuestas.
                 </p>
             </Alert>
         </div>
